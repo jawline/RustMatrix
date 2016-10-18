dcb5c86ea39d33f7c55ebc192b21b1052616a822
= <blake_l@parsed.uk>
2016-10-18 03:40:32 -0700

    Add with_data method

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ pub struct Matrix<T> {
 }
 
 impl<T: Copy> Matrix<T> {
-	pub fn new(cols: usize, rows: usize, initial: T) -> Matrix<T> {
+
+	pub fn new(rows: usize, cols: usize, initial: T) -> Matrix<T> {
 		let data = (0..rows*cols).map(|_| initial.clone()).collect();
 		
 		Matrix {
@@ -17,30 +18,34 @@ impl<T: Copy> Matrix<T> {
 		}
 	}
 
-	pub fn get(&self, col: usize, row: usize) -> T {
+	pub fn with_data(rows: usize, cols: usize, initial: T, old_data: &Vec<T>, data_rows: usize, data_cols: usize) -> Matrix<T> {
+		let mut data: Vec<T> = (0..rows * cols).map(|_| initial.clone()).collect();
+
+		for row in 0..data_rows {
+			for col in 0..data_cols {
+				data[(row * cols) + col] = old_data[(row * data_cols) + col];
+			}
+		}
+
+		return Matrix {
+			data: data,
+			rows: rows,
+			cols: cols
+		}
+	}
+
+	pub fn get(&self, row: usize, col: usize) -> T {
 		self.data[(row * self.cols) + col]
 	}
 
-	pub fn set(&mut self, col: usize, row: usize, val: T) {
+	pub fn set(&mut self, row: usize, col: usize, val: T) {
 		self.data[(row * self.cols) + col] = val;
 	}
 
 	/**
 	 * Expand or contract the matrix, filling any new slots with initial
 	 */
-	pub fn resize(&self, new_cols: usize, new_rows: usize, initial: T) -> Matrix<T> {
-		let mut data: Vec<T> = (0..new_rows*new_cols).map(|_| initial.clone()).collect();
-
-		for x in 0..self.cols {
-			for y in 0..self.rows {
-				data[(y * new_cols) + x] = self.get(x, y);
-			}
-		}
-
-		Matrix {
-			cols: new_cols,
-			rows: new_rows,
-			data: data
-		}
+	pub fn resize(&self, new_rows: usize, new_cols: usize, initial: T) -> Matrix<T> {
+		Matrix::with_data(new_rows, new_cols, initial, &self.data, self.rows, self.cols)
 	}
 }
