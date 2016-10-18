4f1139b89d613626099aa0d98df52b6d15b440be
= <blake_l@parsed.uk>
2016-10-18 03:17:43 -0700

    Implement resize

--- src/lib.rs
+++ src/lib.rs
@@ -24,4 +24,23 @@ impl<T: Copy> Matrix<T> {
 	pub fn set(&mut self, col: usize, row: usize, val: T) {
 		self.data[(row * self.cols) + col] = val;
 	}
+
+	/**
+	 * Expand or contract the matrix, filling any new slots with initial
+	 */
+	pub fn resize(&self, new_cols: usize, new_rows: usize, initial: T) -> Matrix<T> {
+		let mut data: Vec<T> = (0..new_rows*new_cols).map(|_| initial.clone()).collect();
+
+		for x in 0..self.cols {
+			for y in 0..self.rows {
+				data[(y * new_cols) + x] = self.get(x, y);
+			}
+		}
+
+		Matrix {
+			cols: new_cols,
+			rows: new_rows,
+			data: data
+		}
+	}
 }
